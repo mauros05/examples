360d7c41239a8aa108078383df374caf34b288d7
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-17 11:30:27 -0600

    inputs on python

---
+++ input.py
@@ -0,0 +1,11 @@
+name = input("Please enter your name: ")
+print("Your name is " + name + ".")
+print(type(name))
+
+# Challenge
+
+new_name = input("What is your name? ")
+quest = input("What is your quest? ")
+color = input("What is your favorite color? ")
+
+print("So your name is " + name + ", your quest is " + quest + " and your favorite color is " + color)
