6c5adbfba88d343ba3f2d7be24dc03ba4e562e9c
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-17 10:57:31 -0600

    grocery excersie

---
+++ grocery.py
@@ -0,0 +1,11 @@
+penne = 16.68 * 100
+arrabiata = 6.98 * 100
+bag_of_garlic = 16.78 * 100
+italian_seasoning = 15.26 * 100
+artisan_baguettes = 3.00 * 100
+bag_of_meatballs = 4.39 * 100
+
+
+subtotal = (penne + arrabiata + bag_of_garlic + italian_seasoning + artisan_baguettes + bag_of_meatballs) / 100
+
+print(subtotal)
---
+++ variables.py
@@ -0,0 +1,7 @@
+example_1 = 3.14159
+example_2 = 24601
+example_3 = True
+
+print(example_1)
+print(example_2)
+print(example_3)
