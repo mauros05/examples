de1d1ba7a3ae0ffe03e98032a7f06ad4cd15c0ce
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-17 11:24:24 -0600

    More excercises

---
+++ type.py
@@ -0,0 +1,23 @@
+# Type of object
+ex_1 = False
+ex_2 = 20
+ex_3 = 21.1
+
+print(type(ex_1))
+print(type(ex_2))
+print(type(ex_3))
+
+# Pass an object to a String
+
+ex_4 = str(True)
+ex_5 = str(5)
+ex_6 = str(3.21)
+
+print(type(ex_4))
+print(type(ex_5))
+print(type(ex_6))
+
+# Escape
+#  \t \n \' \" \\ 
+
+print("*******\n *****\n  ***\n   *")
