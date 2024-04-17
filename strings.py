0753b93e2fb43c11efac3a9df65598d080a4ba9f
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-17 11:12:46 -0600

    Add String excersie

---
+++ strings.py
@@ -0,0 +1,23 @@
+# CONCATENACION
+
+fruit = "orange"
+print(fruit[2]) # Acceder a un caracter del strgin
+
+# String Slicing
+
+example = "banana"
+print(example[:3])
+print(example[2:5])
+print(example[4:])
+
+
+# Concatenation
+print("pecan" + " " + "pie")
+
+string_example = "Just do it!"
+
+print(string_example[-1])
+print(string_example[5:7])
+print(string_example[8:])
+print(string_example[:4])
+print("Don't " + string_example[5:])
