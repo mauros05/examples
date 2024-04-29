b85725792554c5f27a354ddf4460bf4638fbd620
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-04-29 14:11:42 -0600

    dictionaries

---
+++ dictionaries_excercises.py
@@ -0,0 +1,11 @@
+human = {
+         "name": "Mauricio",
+         "age": 25,
+         "weight": 93,
+         "height": 1.81,
+         "glasses": True
+         }
+
+print(human["weight"])
+print("skin color" in human)
+print("age" not in human)
---
+++ dictionary.py
@@ -0,0 +1,15 @@
+consoles = {
+    "nintento": "Switch",
+    "sony": "Play Station 5",
+    "microsoft": "Xbox Series X"
+}
+
+# Read values from a Dictionary
+
+print(consoles["nintento"])
+
+# To know if a key exist in a dictionary
+
+print("ubiosoft" in consoles)
+
+print("sony" in consoles)
